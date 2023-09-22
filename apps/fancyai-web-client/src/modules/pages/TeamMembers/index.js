6e1e5f7ce92e26dcd36fd590db554190bc86f9cc
Cameron Perrin <perrinwebstudio@gmail.com>
2023-09-22 22:51:58 +0700

    add environment build & buildspec

--- apps/fancyai-web-client/src/modules/pages/TeamMembers/index.js
+++ apps/fancyai-web-client/src/modules/pages/TeamMembers/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { environment } from "../../../environments/environment";
 
 const TeamMembers = () => {
   return (
     <>
-      <h2>Sample page 2</h2>
+      <h2>Sample page 2 {environment.testing}</h2>
       <p>You can kick start your app</p>
     </>
   );
