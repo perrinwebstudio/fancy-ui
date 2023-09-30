1ad63e664da0acc39d2a2e6b7a4de98dd01559d4
Cameron Perrin <perrinwebstudio@gmail.com>
2023-09-30 15:01:56 +0700

    remove required in setup site form

--- apps/fancyai-web-client/src/modules/pages/SiteSetup/Step1.js
+++ apps/fancyai-web-client/src/modules/pages/SiteSetup/Step1.js
@@ -19,20 +19,20 @@ const SiteSetupStep1 = ({ validated }) => {
       style={{marginRight: '10px', color: token.colorSuccess}}
     />} 1. Site Information</StyledSiteSetupTitle>
     
-    <Form.Item required name="name" label="Webiste Name" {...validateProps}>
+    <Form.Item name="name" label="Webiste Name" {...validateProps}>
       <Input placeholder='Apple' suffix={<span />} />
     </Form.Item>
-    <Form.Item required name="url" label="Webiste URL" {...validateProps}>
+    <Form.Item name="url" label="Webiste URL" {...validateProps}>
       <Input placeholder='apple.com' suffix={<span />} />
     </Form.Item>
-    <Form.Item required name="platform" label="Site Platform" {...validateProps}>
+    <Form.Item name="platform" label="Site Platform" {...validateProps}>
       <Select placeholder='Shopify' suffix={<span />}>
         <Select.Option value='shopify'>Shopify</Select.Option>
         <Select.Option value='wordpress'>WordPress</Select.Option>
         <Select.Option value='other'>Other</Select.Option>
       </Select>
     </Form.Item>
-    <Form.Item required name="website_purpose" label="Short description of website and website's purpose" {...validateProps}>
+    <Form.Item name="website_purpose" label="Short description of website and website's purpose" {...validateProps}>
       <TextArea rows={6} />
     </Form.Item>
   </StepFormWrapper>
