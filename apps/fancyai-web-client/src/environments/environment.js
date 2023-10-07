140ab4bdcd383912f7edb42dad56102d61a2596d
Cameron Perrin <perrinwebstudio@gmail.com>
2023-10-08 00:34:59 +0700

    fix gogole client id

--- apps/fancyai-web-client/src/environments/environment.js
+++ apps/fancyai-web-client/src/environments/environment.js
@@ -4,4 +4,5 @@ export const environment = {
   production: false,
   testing: 'Testing Env',
   apiHost: 'https://staging-api.getfancy.ai/api',
+  googleClientId: '1031000066589-88fdhflv7q1a6vn6a731ebjlo4ql1r2e.apps.googleusercontent.com',
 };
--- apps/fancyai-web-client/src/environments/environment.staging.js
+++ apps/fancyai-web-client/src/environments/environment.staging.js
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   testing: 'Testing Staging Env',
   apiHost: 'https://staging-api.getfancy.ai/api',
+  googleClientId: '1031000066589-88fdhflv7q1a6vn6a731ebjlo4ql1r2e.apps.googleusercontent.com',
 };
